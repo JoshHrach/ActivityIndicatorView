862bf64cc499c3ba476ef6be076a1daa22175c9e
Josh Hrach <josh.hrach@gmail.com>
2020-06-01 13:47:34 -0700

    Added test for duration/period

--- Tests/ActivityIndicatorViewTests/ActivityIndicatorViewTests.swift
+++ Tests/ActivityIndicatorViewTests/ActivityIndicatorViewTests.swift
@@ -2,14 +2,10 @@ import XCTest
 @testable import ActivityIndicatorView
 
 final class ActivityIndicatorViewTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ActivityIndicatorView().text, "Hello, World!")
+    func testDurationSetting() {
+        let duration: Double = 2
+        let view = ActivityIndicatorView(period: duration)
+        
+        XCTAssert(view.period == duration)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
