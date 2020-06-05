c8cd6cd98842862d5e2168df5cd8392037a60ad8
Josh Hrach <josh.hrach@gmail.com>
2020-06-05 16:41:55 -0700

    Fixed access

--- Sources/ActivityIndicatorView/View+.swift
+++ Sources/ActivityIndicatorView/View+.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-extension View {
+public extension View {
     func activityIndicator(isVisible: Bool) -> some View {
         self.activityIndicator(isVisible: isVisible) {
             ActivityIndicatorView()
