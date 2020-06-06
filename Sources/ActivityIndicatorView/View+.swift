2eaaff4438799d0f8db80ab5b414f6397b4f14b0
Josh Hrach <josh.hrach@gmail.com>
2020-06-05 17:00:09 -0700

    Added max frame size to default implementation; documentation

--- Sources/ActivityIndicatorView/View+.swift
+++ Sources/ActivityIndicatorView/View+.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 public extension View {
+    /**
+     Adds an activity indicator to this view.
+     
+     This adds a default `ActivityIndicatorView` overlay with a max frame of 75 by 75.
+     
+     - parameter isVisible: Whether or not the activity indicator is visible
+     */
     func activityIndicator(isVisible: Bool) -> some View {
         self.activityIndicator(isVisible: isVisible) {
             ActivityIndicatorView()
         }
     }
     
+    /**
+    Adds an activity indicator to this view.
+     
+     The activity indicator is added as an overlay with a max frame of 75 by 75.
+    
+    - parameter isVisible: Whether or not the activity indicator is visible
+    - parameter indicator: The indicator to overlay on this view
+    */
     func activityIndicator(isVisible: Bool, @ViewBuilder indicator: @escaping () -> ActivityIndicatorView) -> some View {
         Group {
             if isVisible {
                 self
-                .overlay(indicator())
+                    .overlay(indicator().frame(maxWidth: 75, maxHeight: 75))
             } else {
                 self
             }
