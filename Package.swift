13d4e9bb061d59d1252a2dfa5a70516e1fea73c8
Josh Hrach <josh.hrach@gmail.com>
2020-06-12 13:46:26 -0700

    Added swift language version to package file

--- Package.swift
+++ Package.swift
@@ -18,5 +18,6 @@ let package = Package(
         .testTarget(
             name: "ActivityIndicatorViewTests",
             dependencies: ["ActivityIndicatorView"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
